eb30a643adadab698bdb8d8836c3019880367e8d
Artashes A Kocharyan <70996442+JustAnotherDevFromLA@users.noreply.github.com>
2020-10-19 21:55:13 -0700

    Block.java
    Added documentation

--- Block.java
+++ Block.java
@@ -1,30 +1,30 @@
 import java.util.Arrays;
 
 public class Block{
-	
-	private int previousHash;
+	//initiating variables 
+	private int previousHash;	
 	private String[] transactions;
-	
 	private int blockHash;
 	
-	public Block(int previousHash, String[] transactions) {
-		this.previousHash = previousHash;
-		this.transactions = transactions;
+	public Block(int previousHash, String[] transactions) {		
+		// create local variables 
+		this.previousHash = previousHash;	
+		this.transactions = transactions;	
 		
-		Object[] contents = {Arrays.hashCode(transactions), previousHash};
+		Object[] contents = {Arrays.hashCode(transactions), previousHash};	
 		this.blockHash = Arrays.hashCode(contents);
 		
 	}
 	
-	public int getPreviousHash() {
+	public int getPreviousHash() {	//function to retrieve previous block hash number 	
 		return previousHash;
 	}
 	
-	public String[] getTransactions() {
+	public String[] getTransactions() {	//function to retrieve all the tranasction in the block
 		return transactions;
 	}
 
-	public int getBlockHash() {
+	public int getBlockHash() {	//function to return the hash number of a block
 		return blockHash;
 	}		
 	
